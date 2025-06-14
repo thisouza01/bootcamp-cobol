a50fb9e3dfda64d544919a4875089c81aea28f78
Thiago Souza <124224124+thisouza01@users.noreply.github.com>
2025-06-14 09:28:37 -0300

    modulo-incluir

---
+++ projeto/INCLUIR.cob
@@ -0,0 +1,145 @@
+      ******************************************************************
+      * Author:Thiago Souza
+      * Date:09/06/2025
+      * Purpose:Projeto - Sistema de gestao de clientes(CRUD)
+      *  -  MODULO DE INCLUSAO
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION          DIVISION.
+       PROGRAM-ID. INCLUIR.
+      ******************************************************************
+       ENVIRONMENT             DIVISION.
+      *
+       CONFIGURATION           SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA.
+      *
+       INPUT-OUTPUT            SECTION.
+       FILE-CONTROL.
+           SELECT CLIENTES ASSIGN TO "C:/projeto-bootcamp/clientes.dat"
+               ORGANIZATION    IS INDEXED
+               ACCESS MODE     IS DYNAMIC
+               RECORD KEY      IS CHAVE-CLIENTES
+               FILE STATUS     IS FS-CLIENTES.
+      ******************************************************************
+       DATA                    DIVISION.
+      *
+       FILE                    SECTION.
+       FD  CLIENTES.
+       01  REG-CLIENTES.
+           05 CHAVE-CLIENTES.
+               10 REG-TELEFONE     PIC 9(09).
+           05 REG-NOME             PIC A(30).
+           05 REG-EMAIL            PIC X(40).
+      ******************************************************************
+       WORKING-STORAGE         SECTION.
+      *---> STATUS ARQUIVO
+       01  FS-CLIENTES             PIC X(02) VALUE SPACES.
+
+      *---> ABENDS
+       77  WK-ABEND-MESSAGE        PIC X(40) VALUE SPACES.
+
+      *---> TELA
+       77  WK-OPCAO                PIC X     VALUE SPACES.
+       77  WK-TECLA                PIC X     VALUE SPACES.
+       77  WK-MODULO               PIC X(25) VALUE SPACES.
+
+      *---> DATA
+       01  DATA-ATUAL.
+           05 DIA                  PIC 99    VALUE ZEROS.
+           05 FILLER               PIC X     VALUE "/".
+           05 MES                  PIC 99    VALUE ZEROS.
+           05 FILLER               PIC X     VALUE "/".
+           05 ANO                  PIC 9999  VALUE ZEROS.
+      *
+       SCREEN                  SECTION.
+       01  TELA.
+           05 LIMPA-TELA.
+               10 BLANK SCREEN.
+               10 LINE 01 COLUMN 01 PIC X(20) ERASE EOL
+                  BACKGROUND-COLOR 5.
+               10 LINE 01 COLUMN 15 PIC X(20)
+                  BACKGROUND-COLOR 5
+                  FROM "SISTEMA DE CLIENTES".
+               10 LINE 03 COLUMN 02           VALUE "DATA: "
+                   FOREGROUND-COLOR 7.
+               10 LINE 03 COLUMN 08 PIC X(10) USING DATA-ATUAL
+                   FOREGROUND-COLOR 7.
+      *
+       01  MENU-PRINCIPAL.
+           05 LINE 07 COLUMN 15 VALUE "1 - INCLUIR".
+           05 LINE 08 COLUMN 15 VALUE "2 - CONSULTAR".
+           05 LINE 09 COLUMN 15 VALUE "3 - ALTERAR".
+           05 LINE 10 COLUMN 15 VALUE "4 - EXCLUIR".
+           05 LINE 11 COLUMN 15 VALUE "5 - RELATORIO EM TELA".
+           05 LINE 12 COLUMN 15 VALUE "6 - RELATORIO EM DISCO".
+           05 LINE 13 COLUMN 15 VALUE "X - SAIDA".
+           05 LINE 16 COLUMN 15 VALUE "OPCAO........: ".
+           05 LINE 16 COLUMN 28 USING WK-OPCAO.
+      *
+       01  TELA-INCLUSAO.
+           05 BLANK SCREEN.
+           05 LINE 02 COLUMN 01 PIC X(25) ERASE EOL
+               BACKGROUND-COLOR 5 FROM WK-MODULO.
+           05 LINE 08 COLUMN 10 VALUE "INSIRA OS DADOS PARA O CADASTRO".
+      *
+           05 CHAVE FOREGROUND-COLOR 3.
+               10 LINE 10 COLUMN 10 VALUE "TELEFONE ".
+               10 COLUMN PLUS 2 PIC 9(09) USING REG-TELEFONE
+                   BLANK WHEN ZEROS.
+           05 SS-DADOS.
+               10 LINE 11 COLUMN 10 VALUE "NOME.... ".
+               10 COLUMN PLUS 2 PIC X(30) USING REG-NOME.
+               10 LINE 12 COLUMN 10 VALUE "EMAIL... ".
+               10 COLUMN PLUS 2 PIC X(40) USING REG-EMAIL.
+      *
+       01  MOSTRA-ERRO.
+           05 MSG-ERRO.
+               10 LINE 16 COLUMN 10 FROM WK-ABEND-MESSAGE
+               FOREGROUND-COLOR 4.
+               10 COLUMN PLUS 2 PIC X(01) USING WK-TECLA.
+      ******************************************************************
+       PROCEDURE               DIVISION.
+       0000-PRINCIPAL          SECTION.
+             PERFORM 0100-INICIALIZAR.
+             PERFORM 0210-INCLUIR.
+             PERFORM 1000-FINALIZAR.
+
+             STOP RUN.
+       0000-PRINCIPAL-FIM.     EXIT.
+      ******************************************************************
+       0100-INICIALIZAR        SECTION.
+           OPEN I-O CLIENTES.
+           IF FS-CLIENTES EQUAL "35"
+               OPEN OUTPUT CLIENTES
+               CLOSE CLIENTES
+               OPEN I-O CLIENTES
+           END-IF.
+       0100-INICIALIZAR-FIM.   EXIT.
+      *
+       0110-MOSTRA-TELA-INICIAL.
+           MOVE FUNCTION CURRENT-DATE(1:4) TO ANO
+           MOVE FUNCTION CURRENT-DATE(5:2) TO MES
+           MOVE FUNCTION CURRENT-DATE(7:2) TO DIA
+           DISPLAY TELA.
+           ACCEPT  MENU-PRINCIPAL.
+      ******************************************************************
+       0210-INCLUIR.
+           MOVE "MODULO - INCLUSAO " TO WK-MODULO.
+           ACCEPT TELA-INCLUSAO.
+      *---> GRAVA REGISTRO
+           WRITE REG-CLIENTES
+               INVALID KEY
+                   MOVE "CLIENTE JA EXISTE!" TO WK-ABEND-MESSAGE
+                   ACCEPT MOSTRA-ERRO
+                   PERFORM 0110-MOSTRA-TELA-INICIAL
+               NOT INVALID KEY
+                   DISPLAY "CADASTRADO!" AT 1032
+                   FOREGROUND-COLOR 2
+                   ACCEPT WK-TECLA
+                   PERFORM 0110-MOSTRA-TELA-INICIAL
+           END-WRITE.
+      ******************************************************************
+       1000-FINALIZAR          SECTION.
+           CLOSE CLIENTES.
+       1000-FINALIZAR-FIM.     EXIT.
