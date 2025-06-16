72a5abf12fee161e5bc821b655a18ab4580f8d5c
Thiago Souza <124224124+thisouza01@users.noreply.github.com>
2025-06-16 10:52:20 -0300

    adição-copybook-erro

--- projeto/INCLUIR.cob
+++ projeto/INCLUIR.cob
@@ -61,11 +61,7 @@
                10 LINE 12 COLUMN 10 VALUE "EMAIL... ".
                10 COLUMN PLUS 2 PIC X(40) USING REG-EMAIL.
       *
-       01  MOSTRA-ERRO.
-           05 MSG-ERRO.
-               10 LINE 16 COLUMN 10 FROM WK-ABEND-MESSAGE
-               FOREGROUND-COLOR 4.
-               10 COLUMN PLUS 2 PIC X(01) USING WK-TECLA.
+           COPY "ERROR.cpy".
       ******************************************************************
        PROCEDURE               DIVISION.
        0000-PRINCIPAL          SECTION.
